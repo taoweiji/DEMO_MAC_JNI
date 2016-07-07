134e63296bb1cb054ddc646c536912ac078a605f
Wiki <taoweiji2008@qq.com>
2016-07-07 10:16:43 +0800

    Initial Commit

---
+++ DEMO_MAC_JNI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DEMO_MAC_JNI
+//
+//  Created by Wiki on 16/7/7.
+//  Copyright © 2016年 Wiki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
